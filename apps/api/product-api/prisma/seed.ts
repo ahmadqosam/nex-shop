9330094f9560cf468738aefa8a322179a53ecfae
ahmadqosam <ahmadqosam.amrulhaq@gmail.com>
2026-02-08 18:01:11 +0700

    fix build issue

--- apps/api/product-api/prisma/seed.ts
+++ apps/api/product-api/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/product-client';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
